df7e47920700e2f908b91de09dd3368d60bc5535
jefferyyellow <396066193@qq.com>
2017-01-05 16:11:16 +0800

    黄建恒：增加DBJob和LocalLogJob

---
+++ MythCore/GameServer/taskmanager.h
@@ -0,0 +1,50 @@
+#ifndef __TASKMANAGER_H__
+#define __TASKMANAGER_H__
+#include "commontype.h"
+#include "list.h"
+#include "simplelock.h"
+
+enum EmTaskType
+{
+	emTaskType_None			= 0,			// 未知
+	emTaskType_DB			= 1,			// DB
+	emTaskType_LocalLog		= 2,			// 本地日志
+	emTaskType_Login		= 3,			// 登录
+};
+
+using namespace Myth;
+class CInternalMsg;
+/// 数据库管理程序,管理各个数据库Job
+template<uint BaseCount, uint Increment>
+class CTaskManager
+{
+public:
+	CTaskManager(){}
+	~CTaskManager(){}
+
+public:
+	void			pushTask(CInternalMsg* pMsg)
+	{
+		mLock.lock();
+		mTaskList.push_back(pMsg);
+		mLock.unlock();
+	}
+	CInternalMsg*	popTask()
+	{
+		mLock.lock();
+		if (mTaskList.empty())
+		{
+			mLock.unlock();
+			return NULL;
+		}
+		CInternalMsg* pMsg = mTaskList.front();
+		mTaskList.pop_front();
+		mLock.unlock();
+		return pMsg;
+	}
+
+private:
+	CList<CInternalMsg*, BaseCount, Increment>	mTaskList;
+	CSimpleLock						mLock;
+};
+#endif
